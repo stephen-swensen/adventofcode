f2ac7e5cf38227822305b73852e5da790b2431bf
Stephen Swensen <stephen.swensen@gmail.com>
2016-12-01 22:49:20 -0500

    kick off advent of code 2016

---
+++ Advent2016/Day1.fsx
@@ -0,0 +1,35 @@
+﻿#load "Prelude.fsx"
+open System
+
+type Orientation = N | S | E | W
+type Movement = R of int | L of int
+let movements = 
+    let input = readText "input1_part1.txt"
+    input 
+    |> split ", "
+    |> Seq.map (fun movement ->
+        match movement |> Seq.toList with
+        | 'R'::Int'(m) -> R m
+        | 'L'::Int'(m) -> L m)
+    |> Seq.toList
+
+let rec walk movements ((cur_x, cur_y) as cur) orientation =
+    match movements with
+    | [] -> cur
+    | movement::movements' ->
+        match movement with
+        | R m -> 
+            match orientation with
+            | N -> walk movements' (cur_x + m, cur_y) E 
+            | E -> walk movements' (cur_x, cur_y - m) S 
+            | S -> walk movements' (cur_x - m, cur_y) W 
+            | W -> walk movements' (cur_x, cur_y + m) N 
+        | L m -> 
+            match orientation with
+            | N -> walk movements' (cur_x - m, cur_y) W 
+            | W -> walk movements' (cur_x, cur_y - m) S 
+            | S -> walk movements' (cur_x + m, cur_y) E 
+            | E -> walk movements' (cur_x, cur_y + m) N 
+
+let (x,y) = walk movements (0,0) N
+let distance = Math.Abs(x) + Math.Abs(y)
---
+++ Advent2016/Prelude.fsx
@@ -0,0 +1,28 @@
+﻿[<AutoOpen>]
+module Prelude
+
+open System
+open System.IO
+
+do System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let readText path =
+    File.ReadAllText path
+
+let readLines path =
+    File.ReadAllLines path
+
+let split (delimiter:string) (input:string) =
+    let result = input.Split([|delimiter|], StringSplitOptions.None)
+    if result = [|input|] then Seq.empty //nothing was split
+    else upcast result
+
+///Try parse string to int
+let (|Int|_|) str =
+   match System.Int32.TryParse(str) with
+   | (true,int) -> Some(int)
+   | _ -> None
+
+///Try parse char array to int
+let (|Int'|_|) chars =
+   chars |> Seq.toArray |> String |> (|Int|_|)
