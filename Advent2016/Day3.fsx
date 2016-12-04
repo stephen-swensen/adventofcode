06d254efb2dc648c4af067b2abd29605f8ff5f0e
Stephen Swensen <stephen.swensen@gmail.com>
2016-12-03 21:21:53 -0500

    solve day 3 part 2

--- Advent2016/Day3.fsx
+++ Advent2016/Day3.fsx
@@ -10,8 +10,21 @@ let input =
 let possibleTriangle [x;y;z] =
     x + y > z && y + z > x && x + z > y
 
-let part1 =
+let countPossibleTriangles input =
     input
     |> Seq.map possibleTriangle
     |> Seq.filter id
     |> Seq.length
+
+let part1 = countPossibleTriangles input
+
+let part2 =
+    let input' = 
+        let rec loop xl = seq { 
+            match xl with
+            | [] -> ()
+            | [x;y;z]::[x';y';z']::[x'';y'';z'']::xl' ->
+                yield! [[x;x';x''];[y;y';y''];[z;z';z'']] 
+                yield! loop xl' }
+        loop input
+    countPossibleTriangles input'
