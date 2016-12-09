94e1745d956ed65682960aab082308f8fc26cc92
Stephen Swensen <stephen.swensen@gmail.com>
2016-12-09 01:12:52 -0500

    solve day 4 part 2

--- Advent2016/Day4.fsx
+++ Advent2016/Day4.fsx
@@ -9,7 +9,7 @@ let parseLine line =
     let checksum = m.Groups.[3].Value
     room,sector,checksum
 
-let input = readLines "input4_part1.txt" |> Seq.map parseLine
+let input = readLines "input4_part1.txt" |> Seq.map parseLine |> Seq.toList
 
 let genChecksum room =
     room
@@ -21,9 +21,30 @@ let genChecksum room =
     |> Seq.take 5
     |> String.concat "" 
 
-let part1 =
+let realRooms =
     input
+    |> Seq.filter (fun (room, sector, checksum) -> genChecksum room = checksum)
+
+let part1 =
+    realRooms
     |> Seq.sumBy (fun (room, sector, checksum) ->
         let checksum' = genChecksum room
         if checksum' = checksum then sector
         else 0)
+
+let decryptLetter (c:char) times =
+    if c = '-' then ' '
+    else ((((c|>int)+times) - 97) % 26 + 97)|>char;;
+
+let decrypt room sector = 
+    room 
+    |> Seq.map (fun c -> decryptLetter c sector)
+    |> Seq.map string
+    |> String.concat ""
+
+let part2 =
+    realRooms
+    |> Seq.pick (fun (room,sector,_) ->
+        let room' = decrypt room sector
+        if room'.Contains("northpole") then Some(sector)
+        else None)
