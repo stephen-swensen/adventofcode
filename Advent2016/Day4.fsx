9be40aeab82386f5bad979d6d2d5412ebcc936eb
Stephen Swensen <stephen.swensen@gmail.com>
2016-12-09 00:33:12 -0500

    solve day 4 part 1

---
+++ Advent2016/Day4.fsx
@@ -0,0 +1,25 @@
+﻿#load "Prelude.fsx"
+open System
+open System.Text.RegularExpressions
+
+let room = "aaaaa-bbb-zyx" 
+let line = "aaaaa-bbb-z-y-x-123[abxyz]"
+
+let parseLine line =
+    let m = Regex.Match(line, "(.*)-(.*)\[(.*)\]")
+    let room = m.Groups.[1].Value
+    let sector = m.Groups.[2].Value |> int
+    let checksum = m.Groups.[3].Value
+    room,sector,checksum
+
+let genChecksum room =
+    room
+    |> Seq.filter (fun c -> c <> '-')
+    |> Seq.map string
+    |> Seq.countBy id
+    |> Seq.sortBy (fun (letter, cnt) -> -cnt, letter)
+    |> Seq.map fst
+    |> Seq.take 5
+    |> String.concat "" 
+
+genChecksum "not-a-real-room"
